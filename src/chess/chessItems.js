c94070b88fbc085ab2bf73027fe4c81b5a622962
root <root@DESKTOP-MGER0MG.localdomain>
2022-05-27 12:28:12 +0900

    completeAttack
    completeAttack

--- src/chess/chessItems.js
+++ src/chess/chessItems.js
@@ -24,6 +24,8 @@ const CX = "cx";
 const CY = "cy";
 const R = "r";
 
+const INDEX = "index";
+
 const ROWS = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const COLUMNS = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
@@ -63,6 +65,8 @@ const toScale = (scale) => `scale(${scale})`;
 const toTransformScale = (x, y, scale) =>
   `translate(${x}, ${y}) scale(${scale})`;
 
+const calIndex = (row, col) => toRow[row] * boardSize + toCol[col];
+
 const cells = () => {
   const boardHeight = cellSize * (boardSize - 1);
   const result = [];
@@ -78,6 +82,7 @@ const cells = () => {
         x: x,
         y: y,
         color: cellColors[color],
+        index: i * boardSize + j,
         status: null,
       });
     }
@@ -91,9 +96,9 @@ class Board {
     this.select = null;
     this.x = 0;
     this.y = 0;
+    this.index = 0;
     this.turn = WHITE;
     this.cells = cells();
-    this.playCells = cells();
     this.pieceScale = 1;
     this.board = svg
       .attr(ID, BOARD)
@@ -108,23 +113,24 @@ class Board {
       .attr(X, (d) => d.x)
       .attr(Y, (d) => d.y)
       .attr(TRANSFORM, toScale(this.pieceScale))
+      .attr(INDEX, (d) => d.index)
       .on(MOUSEOVER, (e, d) => {
         this.x = d.x;
         this.y = d.y;
         this.show();
-        console.log(d.xi, d.yi, d.status);
+        // console.log(d.xi, d.yi, d.status);
       })
       .on(CLICK, (e, d) => {
-        console.log(d.xi, d.yi, d.status);
+        // console.log(d.xi, d.yi, d.status);
         this.show();
         this.select && this.move(e, d);
-        console.log(d.xi, d.yi, d.status);
+        // console.log(d.xi, d.yi, d.status);
       });
 
     this.circles = d3
       .select(toID(BOARD))
       .selectAll(CIRCLE)
-      .data(this.playCells)
+      .data(this.cells)
       .enter()
       .append(CIRCLE)
       .attr(ID, (d) => C + d.xi + d.yi)
@@ -140,23 +146,27 @@ class Board {
   show = () => console.log(this.x, this.y, this.select);
   move = (event, cell) => {
     const contain = this.select.availableCells.indexOf(event.target.id);
-    this.select.availableCells.map((data) => {
-      d3.select(toCircleID(data)).attr(FILLOPACITY, 0);
-    });
-    this.select.piece.rect.attr(FILLOPACITY, 0);
+    this.select.switchFill();
     if (contain > -1) {
       this.select.piece.svg
         .transition()
         .duration(500)
         .attr(TRANSFORM, toTransformScale(this.x, this.y, this.pieceScale));
-      cell.status = this.turn;
+      this.cells[this.select.currentCellIndex].status = null;
+      this.select.currentCellIndex = cell.index;
+      this.cells[cell.index].status = this.select;
       this.select.coordinate = `${cell.xi}${cell.yi}`;
       this.select.availableCells = this.select.findAvailableCells(
         this.select.coordinate
       );
+      this.switchTurn();
     }
     this.select = null;
   };
+
+  switchTurn() {
+    this.turn = this.turn === WHITE ? BLACK : WHITE;
+  }
 }
 
 const pieceTypes = {
@@ -167,11 +177,15 @@ const pieceTypes = {
 export class Piece {
   constructor(svg, board, coordinate, color, type) {
     this.board = board;
+    this.cells = board.cells;
     this.coordinate = coordinate;
+    this.currentCellIndex = null;
     this.color = color;
     this.type = type;
     this.availableCells = this.findAvailableCells(coordinate);
     this.piece = this.makePiece(svg, color, type, coordinate);
+    this.selected = false;
+    this.fillStatus = 0;
   }
 
   makePiece(svg, color, type, coordinate) {
@@ -184,24 +198,80 @@ export class Piece {
         this.board.pieceScale
       )
     );
+    const index = calIndex(coordinate[1], coordinate[0]);
+    this.currentCellIndex = index;
+    this.board.cells[index].status = this;
+    this.piece.rect.attr(FILL, possiblePlaceColor).on(CLICK, (d, e) => {
+      // Have you already a piece?
+      this.board.turn === this.color &&
+        (() => {
+          // Isn't it same selected piece's color and your color?
+          this.board.select &&
+            this.selected !== this.board.select.selected &&
+            this.board.select.switchFill();
 
-    this.piece.rect.attr(FILL, possiblePlaceColor).on(CLICK, () => {
-      if (this.board.select !== null) {
-        this.piece.rect.attr(FILLOPACITY, 0);
-        this.availableCells.map((cell) => {
-          d3.select(toCircleID(cell)).attr(FILLOPACITY, 0);
-        });
-        this.board.select = null;
-      } else {
-        this.board.select = this;
-        this.piece.rect.attr(FILLOPACITY, 0.5);
-        this.availableCells.map((cell) => {
-          console.log(cell);
-          d3.select(toCircleID(cell)).attr(FILLOPACITY, 0.5);
-        });
-      }
+          this.switchFill();
+
+          this.board.select = this.selected ? this : null;
+        })();
+
+      // Do you select a opponent piece?
+      this.board.turn !== this.color &&
+        this.board.select &&
+        (() => {
+          this.board.select && this.board.select.switchFill();
+          const index = this.board.select.availableCells.indexOf(
+            this.coordinate
+          );
+          if (index > -1) {
+            this.board.select.piece.svg
+              .transition()
+              .duration(500)
+              .attr(
+                TRANSFORM,
+                toTransformScale(
+                  toX(coordinate[0]),
+                  toY(coordinate[1]),
+                  this.board.pieceScale
+                )
+              ) &&
+              this.piece.svg
+                .transition()
+                .duration(500)
+                .attr(
+                  TRANSFORM,
+                  toTransformScale(
+                    toX(coordinate[0]) + svgWidth,
+                    toY(coordinate[1]),
+                    this.board.pieceScale
+                  )
+                );
+            this.cells[this.board.select.currentCellIndex].status = null;
+            this.board.select.currentCellIndex = this.currentCellIndex;
+            this.cells[this.currentCellIndex].status = this.board.select;
+            this.board.select.coordinate = this.coordinate;
+            this.board.select.availableCells =
+              this.board.select.findAvailableCells(this.coordinate);
+            this.board.switchTurn();
+          }
+          this.board.select = null;
+        })();
     });
     return this.piece;
+  }
+
+  switchFill() {
+    this.selected = !this.selected;
+    this.fillStatus = this.selected ? 0.5 : 0;
+    this.piece.rect.attr(FILLOPACITY, this.fillStatus) &&
+      this.availableCells.map((cell) => {
+        let isPiece = this.cells[calIndex(cell[1], cell[0])].status;
+        if (isPiece) {
+          isPiece.piece.rect.attr(FILLOPACITY, this.fillStatus);
+        }
+        d3.select(toCircleID(cell)).attr(FILLOPACITY, this.fillStatus);
+      });
+    return this.selected;
   }
 
   findAvailableCells(coordinate) {
@@ -280,33 +350,33 @@ export class Piece {
           let row = toRow[coordinate[1]];
           let col = toCol[coordinate[0]];
           for (let i = 1; row < ROWS.length && col < COLUMNS.length; i++) {
+            result.push(COLUMNS[col] + ROWS[row]);
             row = row + 1;
             col = col + 1;
-            result.push(COLUMNS[col] + ROWS[row]);
           }
 
           row = toRow[coordinate[1]];
           col = toCol[coordinate[0]];
           for (let i = 1; row < ROWS.length && col >= 0; i++) {
+            result.push(COLUMNS[col] + ROWS[row]);
             row = row + 1;
             col = col - 1;
-            result.push(COLUMNS[col] + ROWS[row]);
           }
 
           row = toRow[coordinate[1]];
           col = toCol[coordinate[0]];
           for (let i = 1; row >= 0 && col < COLUMNS.length; i++) {
+            result.push(COLUMNS[col] + ROWS[row]);
             row = row - 1;
             col = col + 1;
-            result.push(COLUMNS[col] + ROWS[row]);
           }
 
           row = toRow[coordinate[1]];
           col = toCol[coordinate[0]];
           for (let i = 1; row >= 0 && col >= 0; i++) {
+            result.push(COLUMNS[col] + ROWS[row]);
             row = row - 1;
             col = col - 1;
-            result.push(COLUMNS[col] + ROWS[row]);
           }
 
           return result;
@@ -363,44 +433,58 @@ export class Piece {
         return (coordinate) => {
           const result = [];
 
-          let col = toCol[coordinate[0]];
+          let col = toCol[coordinate[0]] + 1;
           let row = toRow[coordinate[1]];
           for (let i = 1; col < COLUMNS.length; i++) {
+            result.push(COLUMNS[col] + ROWS[row]);
             col = col + 1;
-            result.push(COLUMNS[col] + ROWS[row]);
           }
 
-          col = toCol[coordinate[0]];
+          col = toCol[coordinate[0]] - 1;
+          console.log(coordinate);
           for (let i = 1; col >= 0; i++) {
-            col = col - 1;
             result.push(COLUMNS[col] + ROWS[row]);
+            col = col - 1;
           }
 
           col = toCol[coordinate[0]];
-          row = toRow[coordinate[1]];
+          row = toRow[coordinate[1]] + 1;
           for (let i = 1; row < ROWS.length; i++) {
-            row = row + 1;
             result.push(COLUMNS[col] + ROWS[row]);
+            row = row + 1;
           }
 
-          row = toRow[coordinate[1]];
+          row = toRow[coordinate[1]] - 1;
           for (let i = 1; row >= 0; i++) {
-            row = row - 1;
             result.push(COLUMNS[col] + ROWS[row]);
+            row = row - 1;
           }
 
           return result;
         };
       case PAWN:
         return (coordinate) => {
-          let result;
-          const col = toCol[coordinate[0]];
-          const row =
+          let result = [];
+          let col = toCol[coordinate[0]];
+          let row =
             this.color === WHITE
               ? toRow[coordinate[1]] + 1
               : toRow[coordinate[1]] - 1;
-          const cellId = COLUMNS[col] + ROWS[row];
-          result = [cellId];
+          let cellId = COLUMNS[col] + ROWS[row];
+          result.push(cellId);
+
+          if (
+            (coordinate[1] === "2" && this.color === WHITE) ||
+            (coordinate[1] === "7" && this.color === BLACK)
+          ) {
+            row =
+              this.color === WHITE
+                ? toRow[coordinate[1]] + 2
+                : toRow[coordinate[1]] - 2;
+            cellId = COLUMNS[col] + ROWS[row];
+            result.push(cellId);
+          }
+
           return result;
         };
       default:
